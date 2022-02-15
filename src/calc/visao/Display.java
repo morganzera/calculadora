88ee64a8f5d03d74b66f32e3d672eeeb8a9629ed
TalDoMorgan <180075845@aluno.unb.br>
2022-02-15 10:27:09 -0300

    Adicionado classes e configurado a interface do display

---
+++ src/calc/visao/Display.java
@@ -0,0 +1,24 @@
+package calc.visao;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Display extends JPanel {
+
+	private JLabel label;
+
+	public Display() {
+		setBackground(new Color(46, 49, 50));
+		label = new JLabel("1234,56");
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("Futura Bk Bt", Font.PLAIN, 40));
+		
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 15));
+		add(label);
+	}
+}
---
+++ src/calc/visao/Teclado.java
@@ -0,0 +1,11 @@
+package calc.visao;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Teclado extends JPanel {
+
+	public Teclado() {
+
+	}
+}
